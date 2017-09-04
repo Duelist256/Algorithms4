99f2d546c27e4baab0b4c14b800524286c714f5f
Duelist <selykov.iw@yandex.ru>
2017-09-04 23:46:40 +0300

    Change comment

--- src/part1/_2_data_abstraction/exercises/BinarySearch.java
+++ src/part1/_2_data_abstraction/exercises/BinarySearch.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 /**
- * Instrument BinarySearch (page 47) to use a Counter to count the total number
+ * 1.2.9. Instrument BinarySearch (page 47) to use a Counter to count the total number
  * of keys examined during all searches and then print the total after all searches are complete.
  * Hint : Create a Counter in main() and pass it as an argument to rank().
  **/
